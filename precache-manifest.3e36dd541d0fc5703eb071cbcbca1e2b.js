982a4cd36b825b99e4a41a07e6142a42f05605bc
An Moc <thanhanmoc97@gmail.com>
2018-10-23 10:35:36 +0700

    Updates

---
+++ precache-manifest.3e36dd541d0fc5703eb071cbcbca1e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67f13d5c574e8c4e7db2",
+    "url": "/my-flickr/static/js/runtime~main.67f13d5c.js"
+  },
+  {
+    "revision": "bc6ce1c4912679355717",
+    "url": "/my-flickr/static/js/main.bc6ce1c4.chunk.js"
+  },
+  {
+    "revision": "a036bd6d28f87f2ccf13",
+    "url": "/my-flickr/static/js/1.a036bd6d.chunk.js"
+  },
+  {
+    "revision": "bc6ce1c4912679355717",
+    "url": "/my-flickr/static/css/main.a13f87e1.chunk.css"
+  },
+  {
+    "revision": "a036bd6d28f87f2ccf13",
+    "url": "/my-flickr/static/css/1.75b0c82a.chunk.css"
+  },
+  {
+    "revision": "2e77b904516039afedda93a415e6af1b",
+    "url": "/my-flickr/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/my-flickr/precache-manifest.4a712da0619726f72793e9d2b2be8dd1.js"
+  "/my-flickr/precache-manifest.3e36dd541d0fc5703eb071cbcbca1e2b.js"
 );
 
 workbox.clientsClaim();
